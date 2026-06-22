726a76364703168401f8d9edbce4cfd4642b4297
lewpar <lewis.o.parmenter@gmail.com>
2026-06-22 22:12:58 +1000

    Removed old dependency.

--- os.sh
+++ os.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y evtest python3-pip libgl1-mesa-glx libgles2-mesa libegl1-mesa libmtdev1
+sudo apt install -y evtest python3-pip libgl1 libgles2 libegl1 libmtdev1
