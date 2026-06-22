99c8d7ed5d3a2f682bc77e0c4ca9a696848a7960
lewpar <lewis.o.parmenter@gmail.com>
2026-06-22 22:01:10 +1000

    Update with calibration values.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.config import Config
 
 Config.set('graphics', 'show_cursor', '0')
 Config.set('input', 'mouse', 'mouse')
-Config.set('input', 'ads7846', 'hidinput,/dev/input/event9,invert_y=0,min_abs_x=0,max_abs_x=4096,min_abs_y=0,max_abs_y=4096')
+Config.set('input', 'ads7846', 'hidinput,/dev/input/event9,invert_y=0,min_abs_x=150,max_abs_x=3948,min_abs_y=275,max_abs_y=3975')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
