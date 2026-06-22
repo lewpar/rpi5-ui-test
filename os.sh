f886535f997133e16383eb3c8d96fc60eda6e93a
lewpar <lewis.o.parmenter@gmail.com>
2026-06-22 22:15:52 +1000

    Added pip dependency.

--- os.sh
+++ os.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y evtest python3-pip libgl1 libgles2 libegl1 libmtdev1
+sudo apt install -y evtest python3-pip libgl1 libgles2 libegl1 libmtdev1 python3-pip
