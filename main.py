8738f44ac019ae6debe32e266bd1ede991a949b3
lewpar <lewis.o.parmenter@gmail.com>
2026-06-22 21:46:13 +1000

    Added touch settings.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from kivy.config import Config
+
+Config.set('graphics', 'show_cursor', '0')
+Config.set('input', 'mouse', 'mouse')
+Config.set('input', 'ads7846', 'hidinput,/dev/input/event9,invert_y=0,min_abs_x=0,max_abs_x=4096,min_abs_y=0,max_abs_y=4096')
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 
